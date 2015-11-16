8d314f10be3ce2bd04aaa5375b7a29484130812d
Ashley Wu <ashleywu2016@u.northwestern.edu>
2015-11-16 01:01:10 -0600

    scrolling sidebar, need to fix stickyness

--- script.js
+++ script.js
@@ -4,32 +4,39 @@ $(document).ready(function(){
 
 	// build scenes
 	new ScrollMagic.Scene({triggerElement: "#intro"})
-					.setClassToggle("#sub-one", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-one", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#diversity"})
-					.setClassToggle("#sub-two", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-two", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#straight-a-grade-a"})
-					.setClassToggle("#sub-three", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-three", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#campus-culture"})
-					.setClassToggle("#sub-four", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-four", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#growing"})
-					.setClassToggle("#sub-five", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-five", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#self-care"})
-					.setClassToggle("#sub-six", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-six", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	new ScrollMagic.Scene({triggerElement: "#what-can-we-do"})
-					.setClassToggle("#sub-seven", "selected") // add class toggle
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+		.setClassToggle("#sub-seven", "selected") // add class toggle
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
+
+	 $('#sidebar').stickyMojo({footerID: '#footer', contentID: '#story-container'});
+
+	 // $('.sub-side').on('click', function(){
+	 // 	$(this).addClass('selected');
+	 // 	console.log('hey');
+	 // });
 
 });
